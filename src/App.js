0707ae220c1bf4c23792624890d660208982aa52
Nwanochie Emmanuel <emmanuel.nwanochie.197850@unn.edu.ng>
2021-04-08 11:29:15 +0100

    A chat application built using react and chatsocket

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="f9598141-3b07-43ae-856b-224bb2ede386"
+      userName="Nwanoch"
+      userSecret="nwanoch2020"
+    ></ChatEngine>
   );
 }
 
